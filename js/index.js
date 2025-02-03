afbb411e3829666c50090bd728ec9a9671fca4cf
Jay Vaghasiya <jay15994@gmail.com>
2025-02-03 18:22:34 +0530

    Final changes

--- js/index.js
+++ js/index.js
@@ -1,10 +1,35 @@
+const getDelimiter = function(numbers) {
+  if (numbers.startsWith("//")) {
+    return numbers[2];
+  }
+  return ",";
+};
+
+const getNumbers = function(numbers, delimiter) {
+  const newNumbers = numbers.replace(/\\n/g, delimiter);
+
+  if (newNumbers.startsWith("//")) {
+    return newNumbers.substring(4).split(delimiter);
+  }
+  return newNumbers.split(delimiter);
+}
+
 const sum = function(numbersArray) {
   let sum = 0;
+  const negatives = [];
 
   for (let i = 0; i < numbersArray.length; i++) {
     const number = parseInt(numbersArray[i]);
 
-    sum += number;
+    if (number < 0) {
+      negatives.push(number);
+    } else if (number <= 1000) {
+      sum += number;
+    }
+  }
+
+  if (negatives.length > 0) {
+    throw new Error("Negatives not allowed: " + negatives.join(", "));
   }
 
   return sum;
@@ -15,7 +40,8 @@ function add(numbers) {
     return 0;
   }
 
-  const numbersArray = numbers.split(",");
+  const delimiter = getDelimiter(numbers);
+  const numbersArray = getNumbers(numbers, delimiter);
 
   return sum(numbersArray);
 }
@@ -28,8 +54,14 @@ $(document).ready(function() {
   });
 
   $("#calculateButton").click(function() {
+    try {
       const input = $("#inputString").val();
       const result = add(input);
-      $("#result").text("Result: " + result);
+      $("#result").text("Result: " + result)
+                 .removeClass("error");
+    } catch (error) {
+      $("#result").text(error.message)
+                 .addClass("error");
+    }
   });
 });
