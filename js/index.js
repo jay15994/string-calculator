3f4007dcbe45b054ceff2f7d0dc94e07844f8f44
Jay Vaghasiya <jay15994@gmail.com>
2025-02-03 17:43:26 +0530

    sum method added

--- js/index.js
+++ js/index.js
@@ -1,5 +1,23 @@
+const sum = function(numbersArray) {
+  let sum = 0;
+
+  for (let i = 0; i < numbersArray.length; i++) {
+    const number = parseInt(numbersArray[i]);
+
+    sum += number;
+  }
+
+  return sum;
+}
+
 function add(numbers) {
-  return numbers;
+  if (numbers === "") {
+    return 0;
+  }
+
+  const numbersArray = numbers.split(",");
+
+  return sum(numbersArray);
 }
 
 $(document).ready(function() {
