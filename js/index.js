910b1f2864ee4fcd0642db2cff0c2c90da250f2b
Jay Vaghasiya <jay15994@gmail.com>
2025-02-03 17:27:57 +0530

    Enter key handled

--- js/index.js
+++ js/index.js
@@ -3,6 +3,12 @@ function add(numbers) {
 }
 
 $(document).ready(function() {
+  $("#inputString").keypress(function(event) {
+    if (event.which === 13) {
+      $("#calculateButton").click();
+    }
+  });
+
   $("#calculateButton").click(function() {
       const input = $("#inputString").val();
       const result = add(input);
